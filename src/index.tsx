e44b2edc349295059aaf627275f8f833283fb2e9
Eliya Vahl <eliyavahl@gmail.com>
2022-02-03 17:01:09 +0200

    fix: relative path for index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
-import "/projects/blog/src/index.css";
+import "./index.css";
 import App from "./app";
 
 ReactDOM.render(
