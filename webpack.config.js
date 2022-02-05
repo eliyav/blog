77c1b1046a1a0f21505d17a548ffa43d69636d7c
Eliya Vahl <eliyavahl@gmail.com>
2022-02-05 06:53:25 +0200

    fix: favicon display

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Development",
+      favicon: "./favicon.ico",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
